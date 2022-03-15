484891f48252e77557322a86dc4e1ba1c7f4b0f8
Léon Chabbey <leon.chabbey@gmail.com>
2022-03-15 15:27:44 +0100

    changes

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const REGEX = /(0\.0\.0-dev\.([a-zA-Z0-9)]{7}))/g;
+
 try {
   async function run() {
     const githubToken = core.getInput("GITHUB_TOKEN");
     const packagesToken = core.getInput("PACKAGES_TOKEN");
-    console.log({ githubToken, packagesToken });
+    console.log({ githubToken, packagesToken, packagesTokenMissing: !packagesToken });
 
     const octokit = github.getOctokit(githubToken);
     const { issue } = github.context;
-    console.log({ context: github.context });
-    console.log({ issue });
-    const [{ data: pullRequest }, { data: comments }] = await Promise.all([
-      octokit.rest.pulls.get({
-        owner: issue.owner,
-        repo: issue.repo,
-        pull_number: issue.number,
-      }),
-      octokit.rest.issues.listComments({
-        owner: issue.owner,
-        repo: issue.repo,
-        issue_number: issue.number,
-      }),
-    ]);
 
-    console.log({ pullRequest });
+    const commentsQuery = await octokit.rest.issues.listComments({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+    });
+
+    const comments = commentsQuery.data.map((item) => item.body);
     console.log({ comments });
+
+    const versionsToDelete = [];
+    comments.forEach((c) => {
+      const res = c.match(REGEX);
+      if (res) {
+        versionsToDelete.push(...res);
+      }
+    });
+
+    console.log({ versionsToDelete });
+
+    await Promise.all(
+      versionsToDelete.map((v) =>
+        octokit.rest.packages.deletePackageVersionForOrg({
+          package_type: "npm",
+          package_name: issue.repo,
+          org: issue.owner,
+          package_version_id: v,
+        })
+      )
+    );
   }
+
   run();
 } catch (error) {
   core.setFailed(error.message);
