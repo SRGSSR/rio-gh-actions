8934356b065b6edac424d7f7288dce207116b3ae
Léon Chabbey <leon.chabbey@gmail.com>
2022-03-15 11:45:50 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  async function run() {
+    const githubToken = core.getInput("GITHUB_TOKEN");
+    const packagesToken = core.getInput("PACKAGES_TOKEN");
+
+    const octokit = github.getOctokit(githubToken);
+    const { issue } = github.context;
+    const [{ data: pullRequest }, {data: reviewComments}] = await Promise.all([
+      octokit.rest.pulls.get({
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number,
+      }),
+      octokit.rest.pulls.listReviewComments({
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number,
+      }),
+    ]);
+
+    console.log(pullRequest);
+    console.log(reviewComments);
+  }
+  run();
+} catch (error) {
+  core.setFailed(error.message);
+}
